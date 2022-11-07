6b62631db5a91d6f07043ef698afe9d78a85765b
simonpai <simon.pai@gmail.com>
2022-11-07 15:59:17 +0800

    Fix typo.

--- packages/server-commons/src/stream/index.js
+++ packages/server-commons/src/stream/index.js
@@ -1,3 +1,3 @@
 export * from './misc.js';
-export { default as BufferedReadStream } from './buffer.js';
+export { default as BufferedReadStream } from './buffered-read.js';
 export { default as OutputStream } from './output.js';
